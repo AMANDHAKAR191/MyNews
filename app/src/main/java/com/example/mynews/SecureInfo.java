b04b7113419e41a957702c38ea52b022c97ec150
AMANDHAKAR191 <amandhaker191@gmail.com>
2023-01-13 15:19:56 +0530

    initial commit

---
+++ app/src/main/java/com/example/mynews/SecureInfo.java
@@ -0,0 +1,14 @@
+package com.example.mynews;
+
+public class SecureInfo {
+    private String url = "https://newsapi.org/";
+    private String apiKey = "798774a70521453fbc948c757c5cdb77";
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+}
